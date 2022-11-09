47024bd7beb4ab18b891ee1a0c589bca5a32a963
vx007 <vasilyev.x@mail.ru>
2022-11-09 21:45:46 +0300

    Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.HashSet;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        HashSet<Task> tasks = new HashSet<>(15);
+        Random random = new Random();
 
+        while (tasks.size() < 15) {
+            tasks.add(new Task(random.nextInt(1, 9), random.nextInt(1, 9)));
+        }
+
+        for (Task task : tasks) {
+            System.out.println(task);
+        }
     }
 }
